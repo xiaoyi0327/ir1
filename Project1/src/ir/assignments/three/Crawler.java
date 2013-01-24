4b07245833fdd9d1ff0f450182b371ff3cf5075c
Joseph Zhang <xiaoyiz0327@gmail.com>
2013-01-23 19:14:01 -0800

    Pr3

---
+++ Project1/src/ir/assignments/three/Crawler.java
@@ -0,0 +1,18 @@
+package ir.assignments.three;
+
+import java.util.Collection;
+
+public class Crawler {
+	/**
+	 * This method is for testing purposes only. It does not need to be used
+	 * to answer any of the questions in the assignment. However, it must
+	 * function as specified so that your crawler can be verified programatically.
+	 * 
+	 * This methods performs a crawl starting at the specified seed URL. Returns a
+	 * collection containing all URLs visited during the crawl.
+	 */
+	public static Collection<String> crawl(String seedURL) {
+		// TODO implement me
+		return null;
+	}
+}
