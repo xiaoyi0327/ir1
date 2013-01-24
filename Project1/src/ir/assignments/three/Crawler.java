8718fa68331b36d3c835531d19a3cf3abb2c1a64
Joseph Zhang <xiaoyiz0327@gmail.com>
2013-01-23 20:04:15 -0800

    8:04 1/23/2013

--- Project1/src/ir/assignments/three/Crawler.java
+++ Project1/src/ir/assignments/three/Crawler.java
@@ -69,11 +69,4 @@ public void visit(Page page) {
 		return null;
 	}
 	
-	public static void main(String[] args){
-		WebURL url = new WebURL();
-		url.setURL("http://www.ics.uci.edu/");
-		Page page = new Page(url);
-		Crawler crawler = new Crawler();
-		crawler.visit(page);
-	}
 }
